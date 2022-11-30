2d9e9183f3d89ae6c5e19924bd9a45ba0a966b5f
Pietryczko Damian <damian.pietryczko@gmail.com>
2022-11-30 01:52:17 +0100

    Task 19
    Took 50 minutes

--- src/main/java/com/crud/tasks/service/DbService.java
+++ src/main/java/com/crud/tasks/service/DbService.java
@@ -1,11 +1,13 @@
 package com.crud.tasks.service;
 
+import com.crud.tasks.controller.TaskNotFoundException;
 import com.crud.tasks.domain.Task;
 import com.crud.tasks.repository.TaskRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -14,5 +16,11 @@ public class DbService {
 
     public List<Task> getAllTasks() {
         return repository.findAll();
+    }
+    public Task saveTask(final Task task) {
+        return repository.save(task);
+    }
+    public Task getTask(final Long taskId) throws TaskNotFoundException {
+        return repository.findById(taskId).orElseThrow(TaskNotFoundException::new);
     }
 }
