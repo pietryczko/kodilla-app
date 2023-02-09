7ee0f97bff8919db9c366796c5f7270968833022
pietryczko <damian.pietryczko@gmail.com>
2023-02-09 16:31:19 +0100

    Added BadgesDto

--- src/main/java/com/crud/tasks/domain/AttachmentByTypeDto.java
+++ src/main/java/com/crud/tasks/domain/AttachmentByTypeDto.java
@@ -8,15 +8,8 @@ import java.util.List;
 
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class TrelloBoardDto {
+public class AttachmentByTypeDto {
 
-    @JsonProperty("id")
-    private String id;
-
-    @JsonProperty("name")
-    private String name;
-
-    @JsonProperty("lists")
-    private List<TrelloListDto> lists;
-
+    @JsonProperty("trellos")
+    private List<TrelloDto> Trello;
 }
